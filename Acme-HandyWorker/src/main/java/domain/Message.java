9b625f82fc57cb0cab0bc652f3803c7202d78575
MarioBG <Mario.biz.gon@gmail.com>
2018-11-14 23:32:51 +0100

    Resuelto error en merge
    Los métodos getBox y setBox de Message se habían duplicado

--- Acme-HandyWorker/src/main/java/domain/Message.java
+++ Acme-HandyWorker/src/main/java/domain/Message.java
@@ -103,15 +103,4 @@ public class Message extends DomainEntity {
 		this.sender = sender;
 	}
 
-	@Valid
-	@NotNull
-	@ManyToOne(optional = false)
-	public Box getBox() {
-		return this.box;
-	}
-
-	public void setBox(final Box box) {
-		this.box = box;
-	}
-
 }
